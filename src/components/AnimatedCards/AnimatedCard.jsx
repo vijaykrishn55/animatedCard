354fa85efd53ff00cab093b2863f06519abed4de
vijaykrishna <vijaykrishna.top@gmail.com>
2025-04-12 19:00:47 +0530

    improved css

--- src/components/AnimatedCards/AnimatedCard.jsx
+++ src/components/AnimatedCards/AnimatedCard.jsx
@@ -1,24 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../Card/Card";
-import {Data} from "../Data/index"
-import "./AnimatedCard.css";
+import {Data} from "../Data/index";
+import './AnimatedCard.css';
 
 function AnimatedCard() {
- 
   return (
-    <>
-      <div className="animated-card-container">
-        {Data.map((item, index) => {
-          return (
-            <Card
-              key={index}
-              style={{top: `${(index * 10)}px !important`}}
-              {...item}
-            />
-          )
-        })}
-      </div>
-    </>
+    <div className="cards-container">
+      {Data.map((item, index) => (
+        <Card
+          key={index}
+          className="card"
+          style={{ paddingTop: `calc(${index + 1} * 0.5em)` }}
+          {...item}
+        />
+      ))}
+    </div>
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,19 +5,21 @@ function Card({ header, content, link, image }) {
   return (
     <>
       <div className="card">
-        <div className="card-content">
-          <h3 className="card-header">{header}</h3>
-          <p className="card-text">{content}</p>
-          <a
-            href={link}
-            className="card-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn More
-          </a>
+        <div className="card-container">
+          <div className="card-content">
+            <h3 className="card-header">{header}</h3>
+            <p className="card-text">{content}</p>
+            <a
+              href={link}
+              className="card-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn More
+            </a>
+          </div>
+          <img src={image} alt={header} className="card-image" />
         </div>
-        <img src={image} alt={header} className="card-image" />
       </div>
     </>
   );
