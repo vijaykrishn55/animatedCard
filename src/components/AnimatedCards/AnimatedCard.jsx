431cfc89e10eb130807dab9f55e2f8ac50ea2c4a
vijaykrishna <vijaykrishna.top@gmail.com>
2025-05-02 15:57:02 +0530

    mobile responsive

--- src/components/AnimatedCards/AnimatedCard.jsx
+++ src/components/AnimatedCards/AnimatedCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "../Card/Card";
-import {Data} from "../Data/index";
-import './AnimatedCard.css';
+import { Data } from "../Data/index";
+import "./AnimatedCard.css";
 
 function AnimatedCard() {
   return (
@@ -10,7 +10,10 @@ function AnimatedCard() {
         <Card
           key={index}
           className="card"
-          style={{ paddingTop: `calc(${index + 1} * 0.5em)` }}
+          style={{
+            paddingTop: `calc(${index + 1} * 0.5em)`,
+            zIndex: Data.length - index, // <-- Higher z-index for upper cards
+          }}
           {...item}
         />
       ))}
